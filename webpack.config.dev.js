fcdc19eadc34999a7b9e2e81e59f70d6b6f740fd
evilboss <jr.evilboss@gmail.com>
2019-05-08 03:04:47 +0800

    add webpack config

---
+++ webpack.config.dev.js
@@ -0,0 +1,37 @@
+import path from 'path'
+import webpack from 'webpack'
+
+export default {
+    devtools: 'eval-source-map',
+    entry: [
+        'webpack-hot-middleware/client',
+        path.join(__dirname, '/client/index.js')
+    ],
+    output: {
+        path: '/',
+        publicPath: '/'
+    },
+    plugins: [
+        new webpack.NoErrorsPlugin(),
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin()
+    ],
+
+    module: {
+        loaders: [
+            {
+                test: /\.css$/,
+                loaders: ["style-loader", "css-loader"]
+            },
+            {
+                test: /\.js$/,
+                include: path.join(__dirname, 'client'),
+                loaders: ['react-hot', 'babel']
+            }
+
+        ]
+    },
+    resolve: {
+        extentions: ['', '.js']
+    }
+}
