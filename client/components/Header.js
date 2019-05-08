e4f0b6f5ad1e7a5f77444c880eb08915bd3e8152
evilboss <jr.evilboss@gmail.com>
2019-05-08 12:03:04 +0800

    change ul items to readonly checkboxes

--- client/components/Header.js
+++ client/components/Header.js
@@ -33,20 +33,45 @@ class Header extends React.Component {
                             <p>
 
                             </p>
+                            <b>Tasks Done:</b>
                             <ul>
-                                <li><span>Provide a project that can easily be built and tested.</span>
+
+                                <li><input type="checkbox" readOnly checked="checked" className="form-check-input"/>
+                                    <label className="form-check-label">Provide a project that can easily be built and
+                                        tested.</label>
+
                                 </li>
-                                <li><span>Include at least a simple web page to allow testing the API.</span></li>
-                                <li><span>Use ES6.</span></li>
                                 <li>
-                                    <span>Make sure to wrap the functionality of communicating with bitly, in a class.</span>
+                                    <input type="checkbox" readOnly checked="checked" className="form-check-input"/>
+                                    <label className="form-check-label">Include at least a simple web page to allow
+                                        testing the API.</label>
                                 </li>
                                 <li>
-                                    <span>When using packages focus on promise-based packages and not callback-based packages.</span>
+                                    <input type="checkbox" readOnly checked="checked" className="form-check-input"/>
+                                    <label className="form-check-label">Use ES6.</label>
                                 </li>
-                                <li><span
-                                >Feel free to use npm packages for whatever you need and prefer.</span></li>
-                                <li><span>Readability of the code and commenting will be taken into account.</span>
+                                <li>
+                                    <input type="checkbox" readOnly checked="checked" className="form-check-input"/>
+                                    <label className="form-check-label">Make sure to wrap the functionality of
+                                        communicating with bitly, in a class.</label>
+
+                                </li>
+                                <li>
+                                    <input type="checkbox" readOnly checked="checked" className="form-check-input"/>
+                                    <label className="form-check-label">When using packages focus on promise-based
+                                        packages and not callback-based packages.</label>
+                                </li>
+                                <li>
+                                    <input type="checkbox" readOnly checked="checked" className="form-check-input"/>
+                                    <label className="form-check-label">Feel free to use npm packages for whatever you
+                                        need and prefer.</label>
+
+                                </li>
+                                <li>
+                                    <input type="checkbox" readOnly checked="checked" className="form-check-input"/>
+                                    <label className="form-check-label">Readability of the code and commenting will be
+                                        taken into account.</label>
+
                                 </li>
                             </ul>
                         </div>
